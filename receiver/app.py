8d76d01ea2140f570725827be555fcade48b2cb4
karanharika <55457934+karanharika@users.noreply.github.com>
2021-04-14 10:38:51 -0700

    added base_path

--- receiver/app.py
+++ receiver/app.py
@@ -78,7 +78,7 @@ def assign_gate(body):
 
 
 app = connexion.FlaskApp(__name__, specification_dir='')
-app.add_api("openapi.yml")
+app.add_api("openapi.yml", base_path="/receiver", strict_validation=True, validate_responses=True)
 
 if __name__ == "__main__":
     app.run(port=8080)
